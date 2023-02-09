0943cf52361781287c39fe7a1ffb0b1149e19e15
OleksiyZ <106480026+OleksiyZ@users.noreply.github.com>
2023-02-09 14:35:26 +0200

    starting

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
+import { Routes, Route, Link } from 'react-router-dom';
+import { Home } from './Pages/Home';
+import { About } from './Pages/About';
+import { Products } from './Pages/Products';
+import { NotFound } from './Pages/NotFound';
+import { ProductDetails } from './ProductDetails/ProductDetails';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/products">Products</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:productId" element={<ProductDetails />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 };
